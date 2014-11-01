b34059e81f04c254ae5443f64af9e46512a4564e
zmazeh <zaneb_5@msn.com>
2014-11-01 16:38:22 +0000

    Create CrazyDriver.java

---
+++ CrazyDriver.java
@@ -0,0 +1,21 @@
+
+public class CrazyDriver implements Driver{
+	int SpeedAbove = -10; //drives 10 below limit
+	
+	Vehicle auto;
+	public void setSpeedAbove(int a){
+		SpeedAbove = a;
+	}
+	public int getVehicleMax(){
+		return auto.getMaxSpeed();
+	}
+	public Vehicle getVehicle(){
+		return auto;
+	}
+	public void setVehicle(){
+		//TODO
+	}
+	public void setVehicle(Vehicle car){
+		auto = car;
+	}
+}
