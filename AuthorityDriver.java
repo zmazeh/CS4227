9f0e704a96499dbc43db5acecde7cd9f198444ae
zmazeh <zaneb_5@msn.com>
2014-11-01 16:36:03 +0000

    Create AuthorityDriver.java
    Authority Driver: Drives to exact rules of road, enforces speed limit of other drivers whom exceed it.

---
+++ AuthorityDriver.java
@@ -0,0 +1,20 @@
+
+public class AuthorityDriver implements Driver {
+	int SpeedAbove = 0; //drives exactly the speed limit by default
+	Vehicle auto;
+	public void setSpeedAbove(int a){
+		SpeedAbove = a;
+	}
+	public int getVehicleMax(){
+		return auto.getMaxSpeed();
+	}
+	public Vehicle getVehicle(){
+		return auto;
+	}
+	public void setVehicle(){
+		//TODO
+	}
+	public void setVehicle(Vehicle car){
+		auto = car;
+	}
+}
