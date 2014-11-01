39ba92e16c06f5c220d681620e6d387dfa314da9
zmazeh <zaneb_5@msn.com>
2014-11-01 16:40:13 +0000

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,9 @@
+
+public interface Vehicle {
+	
+	public int getDistanceBehind(); //gets distance of car closest behind it
+	public int getDistanceInfront(); //gets distance of car closest infront of it
+	public void setPosition(int a); //sets position on road (in this case one lane)
+	public int getMaxSpeed();
+	public int getPosition();
+}
