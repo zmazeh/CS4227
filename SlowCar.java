e81742ec363348118631ebc44d524373d9000674
zmazeh <zaneb_5@msn.com>
2014-11-01 16:39:45 +0000

    Create SlowCar.java

---
+++ SlowCar.java
@@ -0,0 +1,26 @@
+
+public class SlowCar implements Vehicle {
+	
+	int MaxSpeed = 80;
+	int CurrentPos;
+	
+	public int getDistanceBehind(){
+		//TODO
+		return 0;
+	}
+	public int getDistanceInfront(){
+		//TODO
+		return 0;
+	}
+	public void setPosition(int a){
+		CurrentPos = a;
+	}
+	
+	public int getPosition(){
+		return CurrentPos;
+	}
+	
+	public int getMaxSpeed(){
+		return MaxSpeed;
+	}
+}
