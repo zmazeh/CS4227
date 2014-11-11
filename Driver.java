d50348632af8680d37806fece0e47797cb986221
zmazeh <zaneb_5@msn.com>
2014-11-11 13:19:56 +0000

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -6,4 +6,5 @@ public interface Driver {
 	public Vehicle getVehicle(); //returns Vehicle currently driving
 	public void setVehicle(); //sets to random vehicle unless is authority driver
 	public void setVehicle(Vehicle car); // sets to specified car
+	void setPersonality();
 }
