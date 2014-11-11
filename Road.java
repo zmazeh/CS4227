121fe5b83a82b3d81866b084d70803763208a349
zmazeh <zaneb_5@msn.com>
2014-11-11 20:45:48 +0000

    Create Road.java

---
+++ Road.java
@@ -0,0 +1,11 @@
+public interface Road {
+	
+	public void setSpeedLimit(int a);
+	public int getSpeedLimit();
+	public void setLength();
+	public void getLength();
+	public void addLane(); //in this simulation only 2 lanes per road
+	
+	
+
+}
