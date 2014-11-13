ace21361901fed22ce0a286d40bfb635eda330d2
zmazeh <zaneb_5@msn.com>
2014-11-13 17:24:27 +0000

    Update Car.java

--- Car.java
+++ Car.java
@@ -74,8 +74,8 @@ public abstract class Car {
 	}
 	
 		
-public int getDistanceBehind(); //gets distance of car closest behind it
-public int getDistanceInfront(); //gets distance of car closest infront of it
+public int getDistanceBehind(); //gets distance of car closest behind it (without threading)
+public int getDistanceInfront(); //gets distance of car closest infront of it (without threading)
 public void setPosition(int a); //sets position on road (in this case one lane)
 public int getMaxSpeed();
 public int getPosition();
