02463cf2c80ab1803b5903b13ca15d4334988a33
zmazeh <zaneb_5@msn.com>
2014-11-13 17:23:40 +0000

    Update Car.java
    merge z code with lins

--- Car.java
+++ Car.java
@@ -72,5 +72,13 @@ public abstract class Car {
     	}
     	start();
 	}
+	
+		
+public int getDistanceBehind(); //gets distance of car closest behind it
+public int getDistanceInfront(); //gets distance of car closest infront of it
+public void setPosition(int a); //sets position on road (in this case one lane)
+public int getMaxSpeed();
+public int getPosition();
+void create();
 
 }
